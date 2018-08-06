13e8d25071b7ec122c41388636d464b3488d6ed7
Hector Orellana <hector.orellanaf92@gmail.com>
2018-08-05 23:11:26 -0400

    struct para el estado del servidor

--- src/main.c
+++ src/main.c
@@ -6,7 +6,14 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-int create_server(int port) {
+struct Server {
+  int socket;
+  int port;
+  int clients[1024];
+  int client_count;
+};
+
+int create_server(int port, struct Server *server) {
   int server_socket;
 
   struct sockaddr_in serv_addr, cli_addr;
@@ -33,18 +40,29 @@ int create_server(int port) {
     return -1;
   }
 
-  return server_socket;
+  server->socket = server_socket;
+  server->port = port;
+  server->client_count = 0;
+
+  return 0;
 }
 
-int accept_client(int server_socket) {
+int accept_client(struct Server *server) {
   struct sockaddr_in client_addr;
   int client_size = sizeof(client_addr);
 
-  int client_socket = accept(server_socket,
+  int client_socket = accept(server->socket,
                              (struct sockaddr *)&client_addr,
                              &client_size);
 
-  return client_socket;
+  if (client_socket < 0) {
+    return -1;
+  }
+
+  server->clients[server->client_count] = client_socket;
+  server->client_count++;
+
+  return 0;
 }
 
 int main(int argc, char **argv) {
@@ -57,21 +75,23 @@ int main(int argc, char **argv) {
     port = atoi(argv[1]);
   }
 
-  int server_socket = create_server(port);
+  struct Server server;
 
-  if (server_socket < 0) {
+  if (create_server(port, &server) < 0) {
     perror("Error al iniciar el servidor: ");
     return -1;
   }
 
-  printf("Servidor iniciado y escuchando en el puerto: %d\n", port);
+  printf("Servidor iniciado y escuchando en el puerto: %d\n", server.port);
 
-  int client_socket = accept_client(server_socket);
+  int client_socket = accept_client(&server);
 
   if (client_socket < 0) {
     perror("Error al aceptar un nuevo cliente");
     return -1;
   }
+
+  printf("Nuevo client\n Numero de clientes conectados: %d\n", server.client_count);
 
   return 0;
 }
